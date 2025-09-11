1e9165bcd3922b306b8239060259beabe4c501d3
Michael DiAndrea <michael.a.diandrea@gmail.com>
2025-09-11 13:17:06 -0400

    Add basic Selenium test for python.org search

---
+++ test_python_search.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()  # Make sure chromedriver is in PATH or specify full path
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element(By.NAME, "q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found.", driver.page_source)
+
+    def tearDown(self):
+        self.driver.quit()  # Close the browser
+
+if __name__ == "__main__":
+    unittest.main()
